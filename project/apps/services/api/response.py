e4216f16d73ad53be8862a6a25583ac60538cec9
Muhammad Hammam Islami, S.Kom. <mhi595@ums.ac.id>
2026-05-25 11:30:09 +0700

    [bugfix] custom pagination

--- project/apps/services/api/response.py
+++ project/apps/services/api/response.py
@@ -11,21 +11,23 @@ class CustomPagination(LimitOffsetPagination):
 
     def get_next_link(self):
         next_link = super().get_next_link()
-        if next_link:            
+        hostname_override = getattr(settings, 'HOSTNAME_OVERRIDE', None)
+        if next_link and hostname_override:
             parsed_url = urlparse(next_link)
-            modified_url = parsed_url._replace(netloc=settings.HOSTNAME_OVERRIDE)
+            modified_url = parsed_url._replace(netloc=hostname_override)
             next_link = urlunparse(modified_url)
         return next_link
 
     def get_previous_link(self):
         previous_link = super().get_previous_link()
-        if previous_link:            
+        hostname_override = getattr(settings, 'HOSTNAME_OVERRIDE', None)
+        if previous_link and hostname_override:
             parsed_url = urlparse(previous_link)
-            modified_url = parsed_url._replace(netloc=settings.HOSTNAME_OVERRIDE)
+            modified_url = parsed_url._replace(netloc=hostname_override)
             previous_link = urlunparse(modified_url)
         return previous_link
 
-    def get_paginated_response(self, data):        
+    def get_paginated_response(self, data):
         return Response({
             "status_code": status.HTTP_200_OK,
             "count": self.count,
@@ -34,25 +36,25 @@ class CustomPagination(LimitOffsetPagination):
             "previous": self.get_previous_link(),
             "data": data
         })
-    
+
 
 def custom_response_create(response):
     return Response({
         'status': status.HTTP_201_CREATED,
         'message': 'Data berhasil disimpan',
         'data': response.data
-    })            
+    })
 
 def custom_response_retrieve(response):
     return Response({
         'status': status.HTTP_200_OK,
         'message': 'Data berhasil ditampilkan',
         'data': response.data
-    })      
+    })
 
 def custom_response_update(response):
     return Response({
         'status': status.HTTP_200_OK,
         'message': 'Data berhasil diubah',
         'data': response.data
-    })       
+    })
